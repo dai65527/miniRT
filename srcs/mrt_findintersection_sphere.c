19dde541c8fa3ee523b3c00c74712553787945eb
dnakano <dnakano@student.42tokyo.jp>
2020-11-09 17:40:40 +0900

    Modify: normvec of sphere's intersection (when inside).

--- srcs/mrt_findintersection_sphere.c
+++ srcs/mrt_findintersection_sphere.c
@@ -6,7 +6,7 @@
 /*   By: dnakano <dnakano@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/07 20:29:11 by dnakano           #+#    #+#             */
-/*   Updated: 2020/11/09 13:13:14 by dnakano          ###   ########.fr       */
+/*   Updated: 2020/11/09 17:38:50 by dnakano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,15 @@
 #include "math_utils.h"
 #include "minirt.h"
 
-static double		sphere_calc_dist(t_ray *ray, t_sphere *sphere)
+static double		sphere_calc_dist(t_ray *ray, t_sphere *sphere, int *flg_inside)
 {
 	double		b;
 	double		c;
 	double		d;
-	double		t;
+	double		dist;
 	double		vec[3];
 
+	*flg_inside = 0;
 	math_3dvec_minus(ray->orig, sphere->pos, vec);
 	b = 2.0 * math_3dvec_innerprod(ray->dir, vec);
 	c = math_3dvec_norm(vec);
@@ -29,10 +30,11 @@ static double		sphere_calc_dist(t_ray *ray, t_sphere *sphere)
 	d = b * b - 4.0 * c;
 	if (d < 0)
 		return (-1);
-	if (d == 0)
+	if (mrt_isineps(d))
 		return (-b / 2);
-	else if ((t = (-b - sqrt(d)) / 2.0) > 0)
-		return (t);
+	else if ((dist = (-b - sqrt(d)) / 2.0) > MRT_EPS)
+		return (dist);
+	*flg_inside = 1;
 	return ((-b + sqrt(d)) / 2.0);
 }
 
@@ -40,13 +42,16 @@ static t_surface	sphere_solve(t_ray *ray, t_sphere *sphere)
 {
 	t_surface	surface;
 	double		vec[3];
+	int			flg_inside;
 
-	if ((surface.dist = sphere_calc_dist(ray, sphere)) <= MRT_EPS)
+	if ((surface.dist = sphere_calc_dist(ray, sphere, &flg_inside)) <= MRT_EPS)
 		return (surface);
 	math_3dvec_applylen(ray->dir, surface.dist, vec);
 	math_3dvec_plus(ray->orig, vec, surface.pos);
 	math_3dvec_minus(surface.pos, sphere->pos, surface.normvec);
 	math_3dvec_normalize(surface.normvec, surface.normvec);
+	if (flg_inside)
+		math_3dvec_applylen(surface.normvec, -1.0, surface.normvec);
 	surface.color = sphere->color;
 	return (surface);
 }
